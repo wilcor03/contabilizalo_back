74472c76edab434038129fe34c4de9d28b47a62c
WilCor <wilcor03@gmail.com>
2020-10-28 11:41:39 -0500

    .

--- app/Console/Commands/SendEmail.php
+++ app/Console/Commands/SendEmail.php
@@ -44,9 +44,9 @@ class SendEmail extends Command
 
     $registers = Suscriber::whereNull('times') 
                           //where('times', 1)
-                          //where('email', 'wilcor03@gmail.com')
-                          ->orderBy('id', 'desc')       
-                          ->take((int)$until)                    
+                          ->where('email', 'felipetriana98@gmail.com')
+                          /*->orderBy('id', 'desc')       
+                          ->take((int)$until)                    */
                           ->get(); 
                                                      
     $this->error('Proccess tot: '.count($registers));

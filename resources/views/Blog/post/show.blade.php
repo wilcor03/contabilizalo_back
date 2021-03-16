1a451729efd9e38999a2fc2f83a65086d921bb47
WilCor <wilcor03@gmail.com>
2021-03-15 19:36:51 -0500

    again adsense

--- resources/views/Blog/post/show.blade.php
+++ resources/views/Blog/post/show.blade.php
@@ -85,6 +85,17 @@
 			</div>
 		</article>
 	</section>
+
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<ins class="adsbygoogle"
+	     style="display:block; text-align:center;"
+	     data-ad-layout="in-article"
+	     data-ad-format="fluid"
+	     data-ad-client="ca-pub-6749239594655834"
+	     data-ad-slot="3583140684"></ins>
+	<script>
+	     (adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
 	
 	@if(count($post->videos))
 	<section>

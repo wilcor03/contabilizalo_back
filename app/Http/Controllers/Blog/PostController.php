793016966da75b1bf814c163a0d735e5c87825e1
WilCor <wilcor03@gmail.com>
2021-05-02 14:20:43 -0500

    .

--- app/Http/Controllers/Blog/PostController.php
+++ app/Http/Controllers/Blog/PostController.php
@@ -285,8 +285,10 @@ class PostController extends Controller
     }   
     
     $this->saveRegisters("CURSO VBA: ");
-    return redirect()->to('https://www.udemy.com/course/de-0-a-experto-en-macros-y-vba-excel-facil-para-humanos/?referralCode=E7FC50E0FB91493D69A3');
-    //https://go.hotmart.com/H42856436B
+    return redirect()->to('https://go.hotmart.com/H42856436B');
+    //
+    //udemy
+    //https://www.udemy.com/course/de-0-a-experto-en-macros-y-vba-excel-facil-para-humanos/?referralCode=E7FC50E0FB91493D69A3
     
 
     //return view('Blog.promo.excel-vba');

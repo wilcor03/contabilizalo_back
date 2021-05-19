ca0990fa710edf66ab69d203066685a5508b66f8
WilCor <wilcor03@gmail.com>
2021-05-19 17:36:57 -0500

    prueba

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,9 @@
 Route::get('/', function(){
 	return "<h1 style='margin: 0 auto 0; padding:10px;'>SITIO EN MANTENIMIENTO... VOLVEVEMOS PRONTO!!!</h1>";
 });
+
+Route::get('/test', function(){
+	return "running";
+});
+
 Route::get('apps/dian/rut-consult/{cc}', 'DianAppController@rutConsult');

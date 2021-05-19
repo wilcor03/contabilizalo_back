378d5fea82017608d3f8537b62e1f6c4b67797a7
WilCor <wilcor03@gmail.com>
2021-05-18 20:46:03 -0500

    rutas borrads

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,5 @@
 <?php
-
+Route::get('/', function(){
+	return "<h1>SITIO EN MANTENIMIENTO... VOLVEVEMOS PRONTO!!!</h1>";
+});
 Route::get('apps/dian/rut-consult/{cc}', 'DianAppController@rutConsult');

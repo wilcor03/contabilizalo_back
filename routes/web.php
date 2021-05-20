e865cf7dfbeeb5e370e32a91e0b19d197cede12a
WilCor <wilcor03@gmail.com>
2021-05-20 09:59:06 -0500

    .

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,162 @@
 <?php
-Route::get('/', function(){
-	return "<h1 style='margin: 0 auto 0; padding:10px;'>SITIO EN MANTENIMIENTO... VOLVEVEMOS PRONTO!!!</h1>";
+Route::group(['middleware' => 'auth.basic'], function(){
+
+	Route::group(['prefix' => 'admin'], function(){
+		Route::get('administracion', ['as' => 'dashboard', 'uses' => 'UserController@dashboard']);
+		
+		Route::group(['namespace' => 'Blog'], function() {
+			/*
+			!-----------------------------------------------
+			!	POST ADMIN
+			!----------------------------------------------
+			*/
+			Route::get('posts', ['as' => 'post.view.all', 'uses' => 'PostController@viewAll']);
+			Route::get('post/create', ['as' => 'post.create', 'uses' => 'PostController@create']);
+			Route::post('post', ['as' => 'post.store', 'uses' => 'PostController@store']);
+			Route::get('post/{post}/edit', ['as' => 'post.edit', 'uses' => 'PostController@edit']);
+			Route::put('post/{post}', ['as' => 'post.update', 'uses' => 'PostController@update']);
+			Route::get('post-search/{post?}', ['as' => 'post.search', 'uses' => 'PostController@search']);
+			Route::post('add-recommended-post/{post}/{editingPost?}', ['as' => 'post.recommended', 'uses' => 'PostController@addRecommended']); //add or delete a recommend post
+			Route::get('post-recommended-list/{editingPost?}', ['as' => 'post.recommended.lists', 'uses' => 'PostController@listRecommendedPosts']);//posts attached
+			Route::get('post/cache/delete/{url?}', ['as' => 'post.cache.delete', 'uses' => 'PostController@deleteAllCache']);//posts attached
+			
+			/*
+			!-----------------------------------------------
+			!	CATEGORY ADMIN
+			!----------------------------------------------
+			*/
+			Route::get('/{category}/c', ['as' => 'category.show', 'uses' => 'CategoryController@show']);
+
+			/*
+			!-----------------------------------------------
+			!	VIDEO ADMIN
+			!----------------------------------------------
+			*/
+			Route::post('video/{post?}', ['as' => 'video.store', 'uses' => 'VideoController@store']);
+			Route::get('video-by-post/{post?}', ['as' => 'video.by.post', 'uses' => 'VideoController@byPost']);
+			Route::delete('video/{video}', ['as' => 'video.destroy', 'uses' => 'VideoController@destroy']);
+			Route::get('new-token', ['as' => 'new.token', 'uses' => 'VideoController@newToken']);
+
+			/*
+			!-----------------------------------------------
+			!	FILES ADMIN
+			!----------------------------------------------
+			*/
+			Route::post('file/{post?}', ['as' => 'file.store', 'uses' => 'FileController@store']);
+			Route::get('files-by-post/{post?}', ['as' => 'file.by.post', 'uses' => 'FileController@byPost']);
+			Route::delete('file/{file}', ['as' => 'file.destroy', 'uses' => 'FileController@destroy']);			
+
+			/*
+			!---------------------------------------------
+			! IMAGES
+			!----------------------------------------------
+			*/
+			Route::post('image/{editingPost?}', ['as' => 'image.store', 'uses' => 'ImageController@store']);
+			Route::get('image-by-post/{editingPost?}', ['as' => 'image.by.post', 'uses' => 'ImageController@byPost']);
+			Route::delete('image/{image}', ['as' => 'image.destroy', 'uses' => 'ImageController@destroy']);
+
+			/*
+			!------------------------------------------
+			! VARIOUS
+			!--------------------------------------------*/
+			Route::get('get-url', 'PostController@getUrl');
+
+		});
+	});
+
 });
 
-Route::get('/test', function(){
-	return "running";
+/*----------------------------
+! PUBLIC ROUTES
+-----------------------------*/
+Route::get('emails', function(){
+	$suscriber = App\Suscriber::find(1);
+	//dd($suscriber);
+	return (new App\Mail\DetripsRaffleEmail($suscriber))->render();
 });
+
+/*Route::get('redirecting-to-detrips', 'UserController@clickOnEmail')->name('redir.detrips');*/
+
+Route::get('/', ['as' => 'home', 'uses' => 'UserController@home']);
+
+Route::group(['namespace' => 'Blog'], function() {
+	/*
+	!	------------------------------------
+	! POSTS
+	!--------------------------------------
+	*/
+	Route::get('/{post}', ['as' => 'post.show', 'uses' => 'PostController@show']);
+	/*
+	!	------------------------------------
+	! SITEMAP
+	!--------------------------------------
+	*/
+	Route::get('sitemap/xml', ['as' => 'sitemap', 'uses' => 'SitemapController@sitemap']);
+	/*
+	!	------------------------------------
+	! FILES
+	!--------------------------------------
+	*/
+	Route::get('file-download/{file}', ['as' => 'file.download', 'uses' => 'FileController@download']);
+
+	/*
+	|--------------------------------------
+	| PROMO - VENTAS
+	|--------------------------------------
+	*/
+
+	Route::get('promo/descargar-aplicativo-renta-personas-naturales', ['as' => 'promo.apl.renta', 'uses' => 'PostController@promoPersNaturales']);
+
+	Route::get('promo/salir-de-la-lista', ['as' => 'promo.delete.suscriber', 'uses' => 'PostController@deleteSuscriber']);
+
+	Route::get('promo/descargar-aplicativo-renta-personas-juridicas', ['as' => 'promo.apl.renta.j', 'uses' => 'PostController@promoApl']);
+
+	Route::get('promo/venta-de-cursos', ['as' => 'promo.courses', 'uses' => 'PostController@promoCourses']);
+
+	Route::get('promo/curso-excel-macros-vba', ['as' => 'promo.excel-vba', 'uses' => 'PostController@promoExcel']);
+
+	/*
+	|-------------------
+	| WHO WE ARE
+	|--------------------	
+	*/
+
+	Route::get('conta/quienes-somos', 'PostController@whoWeAre');
+	
+	/*
+	|--------------------------------------
+	| PAYU - PAGE STATES OF PAYMENTS
+	|--------------------------------------
+	*/
+
+	Route::get('payu/pago-exitoso', ['as' => 'payu.success', 'uses' => 'PostController@payStates']);
+	Route::get('payu/pago-no-exitoso', ['as' => 'payu.no.success', 'uses' => 'PostController@payStates']);
+	Route::get('payu/pago-pendiente', ['as' => 'payu.pending', 'uses' => 'PostController@payStates']);
+
+	/*
+	|---------------------------------------
+	| COURSES
+	|---------------------------------------
+	*/
+	
+	Route::get('cursos/certificados', 'VariousController@cursos');
+	//Route::post('cursos/contact', 'VariousController@contact');
+	/*Route::get('cursos/detalle/{slug}', 'VariousController@courseDetail')->name('course.detail');*/
+});
+#### WEBINARS
+Route::get('/examen/soy-contabilizer', 'Blog\VariousController@certification')->name('exam.show');
+
+Route::post('/examen/soy-contabilizer', 'Blog\VariousController@certifPost')->name('exam.store');
+Route::get('examen/certificacion', 'Blog\VariousController@verifyCertification')->name('exam.verify-certif');
+
+###### PREINSCRITOS
+Route::any('cursos/preinscritos', 'Blog\VariousController@preinscritos')->name('preins.go');
+
+/*Route::get('courses/test', function(){	
+	$user = App\User::find(1);	
+  return (new App\Mail\DetripsRaffleEmail($user))->render(); 
+});*/
+
+### APP DIAN
 
 Route::get('apps/dian/rut-consult/{cc}', 'DianAppController@rutConsult');
